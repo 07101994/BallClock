b76ca64bb67ffd6b7ceefc585ee7f3e58fade108
Mark Miller <developmentalmadness@gmail.com>
2014-02-05 07:03:41 -0700

    added dotnet solution

---
+++ dotnet/BallClockDotNet/Clock.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallClockDotNet
+{
+	public class Clock
+	{
+		private Stack<int> ones = new Stack<int>(5);
+		private Stack<int> fives = new Stack<int>(12);
+		private Stack<int> hours = new Stack<int>(13);
+
+		private Queue<int> queue = new Queue<int>();
+
+		private int cycles = 0;
+		private int size = 0;
+
+		private Action<int[], Clock> onCycle;
+
+		public Clock(int size, Action<int[], Clock> onCycle)
+		{
+			this.size = size;
+			this.onCycle = onCycle;
+
+			hours.Push(-1);
+
+			for (int i = 1; i <= size; i++)
+				queue.Enqueue(i);
+		}
+
+		public void Increment()
+		{
+			ones.Push(queue.Dequeue());
+
+			if (ones.Count < 5) return;
+
+			fives.Push(ones.Pop());
+			while (ones.Count > 0)
+				queue.Enqueue(ones.Pop());
+
+			if (fives.Count < 12) return;
+
+			var hour = fives.Pop();
+			while (fives.Count > 0)
+				queue.Enqueue(fives.Pop());
+
+			if (hours.Count < 12)
+			{
+				hours.Push(hour);
+			}
+			else
+			{
+				while (hours.Count > 1)
+					queue.Enqueue(hours.Pop());
+			
+				// 13th ball goes on last
+				queue.Enqueue(hour);
+
+				RaiseOnCycle();
+			}
+		}
+
+		private void RaiseOnCycle()
+		{
+			cycles++;
+
+			if (onCycle != null)
+			{
+				int[] order = new int[size];
+				for (int i = 0; i < size; i++)
+				{
+					order[i] = queue.Dequeue();
+					queue.Enqueue(order[i]);
+				}
+
+				onCycle.Invoke(order, this);
+			}
+		}
+
+		public TimeSpan Time
+		{
+			get
+			{
+				int minutes = (fives.Count * 5) + ones.Count;
+				return new TimeSpan(hours.Count, minutes, 0);
+			}
+		}
+
+		public int Days
+		{
+			get
+			{
+				return cycles / 2;
+			}
+		}
+
+		public int Cycles { get { return cycles; } }
+	}
+}
---
+++ dotnet/BallClockDotNet/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallClockDotNet
+{
+	class Program
+	{
+		// index of the greatest common divisor of each ball
+		private static int[] gcdIndex;
+		private static int gcdNeeded;
+
+
+		static void Main(string[] args)
+		{
+			int size = 0;
+
+			while (int.TryParse(Console.ReadLine(), out size))
+			{
+				if (size == 0) break;
+				if (size >= 27 && size <= 127)
+				{
+					gcdIndex = new int[size];
+					gcdNeeded = size;
+					Clock target = new Clock(size, CheckIndex);
+					while (gcdNeeded > 0)
+						target.Increment();
+					PrintResult(size);
+				}
+			}
+		}
+
+		private static void PrintResult(int size)
+		{
+			var result = 1;
+			var lcdSet = gcdIndex.Distinct().ToArray();
+
+			foreach (var lcd in lcdSet)
+				result = LCM(result, lcd);
+
+			Console.WriteLine("{0} balls cycle after {1} days.", size, result / 2);
+		}
+
+		public static int LCM(int a, int b)
+		{
+			int max = Math.Max(a, b);
+			int min = Math.Min(a, b);
+
+			for (int i = 1; i <= min; i++)
+				if ((max * i) % min == 0)
+					return i * max;
+
+			return min;
+		}
+
+		private static void CheckIndex(int[] order, Clock clock)
+		{
+			if (gcdNeeded == 0) return;
+
+			for (int i = 0; i < order.Length; i++)
+			{
+				if (order[i] == i + 1 && gcdIndex[i] == 0)
+				{
+					gcdNeeded--;
+					gcdIndex[i] = clock.Cycles;
+				}
+			}
+		}
+	}
+}
